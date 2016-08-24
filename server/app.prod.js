8b9d50c173067bb3974962769e44ed5efee72d5d
alexlbr <alex.lobera@gmail.com>
2016-08-24 11:30:31 +0100

    improved app.prod.js

--- server/app.prod.js
+++ server/app.prod.js
@@ -1,8 +1,70 @@
 import webpack from 'webpack';
 import config from '../webpack.prod.config';
 import express from 'express';
+import log4js from 'log4js';
+import reactApp from './app';
 
 const app = express();
 const compiler = webpack(config);
 
-// To be implemented
+const logger = log4js.getLogger();
+logger.setLevel('TRACE');
+
+// @TODO move this to a config file
+properties = {
+	application: {
+		'port-https': 443,
+		'port': 80
+	},
+	env: 'prod'
+};
+
+if (properties.env === 'prod') {
+  log4js.configure({
+    appenders: [
+      {
+        type: 'console',
+        layout: { type: 'basic' },
+      },
+    ], replaceConsole: true,
+  });
+}
+
+/**
+ *   Config logger
+ */
+app.use(log4js.connectLogger(logger, {
+  level: log4js.levels.DEBUG,
+  format: ':method :url - :status - :response-time ms',
+}));
+
+/**
+ *   React app
+ */
+app.use(reactApp);
+
+
+/**
+ *   server - http
+ */
+const server = app.listen(properties.application.port, () => {
+  const { address, port } = server.address();
+  logger.info(`Environment = ${properties.env}`);
+  logger.info(`Api listening at http://${address}:${port}`);
+});
+
+
+/**
+ *   server - https
+ */
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync('./config/certificates/key.pem'),
+    cert: fs.readFileSync('./config/certificates/fullchain.pem'),
+  },
+  app
+).listen(properties.application['port-https'], () => {
+  const { address, port } = httpsServer.address();
+  logger.info(`Environment = ${properties.env}`);
+  logger.info(`Api listening at https://${address}:${port}`);
+});
