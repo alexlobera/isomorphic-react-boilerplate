0e9d28533f08536a18fa6042f8ecb37ba9b4edb4
alexlbr <alex.lobera@gmail.com>
2016-08-24 11:07:37 +0100

    added build:client and build:server

--- webpack.base.js
+++ webpack.base.js
@@ -10,7 +10,7 @@ module.exports = {
     extensions:         ['', '.js', '.jsx']
   },
   output: {
-    path:       path.join(__dirname, 'dist'),
+    path:       path.join(__dirname, 'dist/client'),
     filename:   'bundle.js',
     publicPath: '/'
   },
